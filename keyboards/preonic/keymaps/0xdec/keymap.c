55731725dac369cd75dbd8f426d894758ac43b1a
Jordi Pakey-Rodriguez <jordi@0xdec.im>
2020-04-04 20:07:04 -0700

    Swap Colemak and QWERTY layers

--- keyboards/preonic/keymaps/0xdec/keymap.c
+++ keyboards/preonic/keymaps/0xdec/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 
 enum preonic_layers {
-  _COLEMAK,
   _QWERTY,
+  _COLEMAK,
   _GAMENUM,
   _FN,
   _PN,
@@ -10,35 +10,14 @@ enum preonic_layers {
 };
 
 enum preonic_keycodes {
-  COLEMAK = SAFE_RANGE,
-  QWERTY,
+  QWERTY = SAFE_RANGE,
+  COLEMAK,
   GAMENUM,
   FN,
   PN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-/* Colemak
- *,-----------------------------------------------------------------------------------------------.
- *|   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |   0   |   -   |   =   |
- *|-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- *|   `   |   Q   |   W   |   F   |   P   |   G   |   J   |   L   |   U   |   Y   |   ;   |   \   |
- *|-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- *| CTRL  |   A   |   R   |   S   |   T   |   D   |   H   |   N   |   E   |   I   |   O   |   '   |
- *|-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- *| SHIFT |   Z   |   X   |   C   |   V   |   B   |   K   |   M   |   ,   |   .   |   /   | ENTER |
- *|-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- *|  ESC  |  FN   |  GUI  |  ALT  |  TAB  | BKSPC | SPACE |  PN   | LEFT  | DOWN  |  UP   | RIGHT |
- *`-----------------------------------------------------------------------------------------------'
- */
-[_COLEMAK] = LAYOUT_preonic_grid( \
-  KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,KC_EQL , \
-  KC_GRV ,KC_Q   ,KC_W   ,KC_F   ,KC_P   ,KC_G   ,KC_J   ,KC_L   ,KC_U   ,KC_Y   ,KC_SCLN,KC_BSLS, \
-  KC_LCTL,KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_D   ,KC_H   ,KC_N   ,KC_E   ,KC_I   ,KC_O   ,KC_QUOT, \
-  KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_K   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_ENT , \
-  KC_ESC ,FN     ,KC_LGUI,KC_LALT,KC_TAB ,KC_BSPC,KC_SPC ,PN     ,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT  \
-),
 
 /* QWERTY
  *,-----------------------------------------------------------------------------------------------.
@@ -58,6 +37,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSLS, \
   KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT, \
   KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_ENT , \
+  KC_ESC ,FN     ,KC_LGUI,KC_LALT,KC_TAB ,KC_BSPC,KC_SPC ,PN     ,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT  \
+),
+
+/* Colemak
+ *,-----------------------------------------------------------------------------------------------.
+ *|   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |   0   |   -   |   =   |
+ *|-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
+ *|   `   |   Q   |   W   |   F   |   P   |   G   |   J   |   L   |   U   |   Y   |   ;   |   \   |
+ *|-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
+ *| CTRL  |   A   |   R   |   S   |   T   |   D   |   H   |   N   |   E   |   I   |   O   |   '   |
+ *|-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
+ *| SHIFT |   Z   |   X   |   C   |   V   |   B   |   K   |   M   |   ,   |   .   |   /   | ENTER |
+ *|-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
+ *|  ESC  |  FN   |  GUI  |  ALT  |  TAB  | BKSPC | SPACE |  PN   | LEFT  | DOWN  |  UP   | RIGHT |
+ *`-----------------------------------------------------------------------------------------------'
+ */
+[_COLEMAK] = LAYOUT_preonic_grid( \
+  KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,KC_EQL , \
+  KC_GRV ,KC_Q   ,KC_W   ,KC_F   ,KC_P   ,KC_G   ,KC_J   ,KC_L   ,KC_U   ,KC_Y   ,KC_SCLN,KC_BSLS, \
+  KC_LCTL,KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_D   ,KC_H   ,KC_N   ,KC_E   ,KC_I   ,KC_O   ,KC_QUOT, \
+  KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_K   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_ENT , \
   KC_ESC ,FN     ,KC_LGUI,KC_LALT,KC_TAB ,KC_BSPC,KC_SPC ,PN     ,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT  \
 ),
 
